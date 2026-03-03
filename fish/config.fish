271b3f632175a7a2d9a507c01461cb59d2023d04
shiroanon <dubeyshashwat9898@gmail.com>
2026-03-03 13:48:46 +0530

    add stuffs

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,17 @@ if status is-interactive
     alias s="pacman -Ssq"
     alias arch="uwufetch"
     alias ghrc ="gh repo create"
+    atuin init fish | source
 end
-
+function frg
+    # The 'fzf' command with 'ripgrep' as the engine
+    fzf --ansi --disabled \
+        --bind "start:reload:rg --column --line-number --no-heading --color=always --smart-case -- ''" \
+        --bind "change:reload:sleep 0.1; rg --column --line-number --no-heading --color=always --smart-case -- {q} || true" \
+        --delimiter : \
+        --preview 'bat --color=always --style=numbers --highlight-line {2} {1}' \
+        --preview-window 'right,60%,border-bottom,+{2}+3/3' \
+        --bind 'enter:become(nvim {1} +{2})'
+end
 # uv
 fish_add_path "/home/shiro/.local/bin"
